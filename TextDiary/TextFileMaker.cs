3e5714c3ab3f2741a9e67d34d6f80ce8ff60611c
Akn <sibata486@gmail.com>
2018-12-21 16:42:30 +0900

    新しくTextFileMakerクラスを追加。一部のメソッドを試験的に宣言。

---
+++ TextDiary/TextFileMaker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDiary {
+    class TextFileMaker {
+        private String currentDirectoryPath = "";
+        public String separateCharacter = "-";
+
+        public TextFileMaker( String setCurrentDirectoryPath ) {
+            currentDirectoryPath = setCurrentDirectoryPath;
+        }
+
+        public void createTextFile( String writingText ) {
+
+        }
+
+        public String getDataString() {
+            DateTime date = System.DateTime.Now;
+            return date.ToString();
+        }
+
+        public String separateLine {
+            get {
+                String line = "";
+                const int SEPARATE_LENGTH = 5;
+                for(int i = 0; i < SEPARATE_LENGTH; i++) {
+                    line += separateCharacter;
+                }
+                return line;
+            }
+        }
+    }
+}
