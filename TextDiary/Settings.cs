669a1f125dd28bbd2309444cce2184221b5ae05c
Akn <sibata486@gmail.com>
2018-12-28 19:54:01 +0900

    アプリケーションの設定に関する変数を追加

--- TextDiary/Settings.cs
+++ TextDiary/Settings.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace TextDiary {
-    class Setting {
+
+    class Settings {
+
+        public String currentDirectoryPath;
+        public Point windowLocation;
+        public Point windowSize;
+
     }
 }
