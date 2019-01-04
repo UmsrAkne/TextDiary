d07a77c58ce7259d923f4e54f43dff916d874db9
Akn <sibata486@gmail.com>
2019-01-04 20:49:18 +0900

    機能追加。キーボードイベントを設置して、キーボード入力でテキストファイルを生成可能にした

--- TextDiary/Form1.cs
+++ TextDiary/Form1.cs
@@ -10,12 +10,21 @@ namespace TextDiary {
 
         public Form1() {
             InitializeComponent();
+            azukiControl.KeyDown += this.keyboardEventHandler;
         }
 
         private void Form1_Load(object sender, EventArgs e) {
             settings = new Settings();
             settings.currentDirectoryPath = System.IO.Directory.GetCurrentDirectory() + "\\text";
             textFileMaker = new TextFileMaker(settings.currentDirectoryPath);
+        }
+
+        private void keyboardEventHandler(object sender , KeyEventArgs e) {
+            if (e.Control == true && e.KeyCode == Keys.Enter) {
+                textFileMaker.createTextFile(azukiControl.Text);
+                azukiControl.Text = "";
+                e.Handled = true;
+            }
         }
     }
 }
