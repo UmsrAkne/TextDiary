56ac80d4281ca2bbdf58a918adf7ec4c2068fc86
Akn <sibata486@gmail.com>
2019-03-18 01:51:55 +0900

    [クラス追加]　Todoの内容をひとまとめで表すためにTodoクラスを追加。

---
+++ TextDiary/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDiary {
+    class Todo {
+
+        public String AdditionDate;
+        public String content;
+        public String period;
+        public bool isCompleted = false;
+    }
+}
