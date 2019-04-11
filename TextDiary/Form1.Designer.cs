af4536e6e7cfbac6e009d369fc9a96d129d720d3
Akn <sibata486@gmail.com>
2019-04-12 02:00:42 +0900

    [デザイン変更]　ウィンドウの初期サイズを大きめに変更。

--- TextDiary/Form1.Designer.cs
+++ TextDiary/Form1.Designer.cs
@@ -237,7 +237,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.ClientSize = new System.Drawing.Size(784, 562);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
