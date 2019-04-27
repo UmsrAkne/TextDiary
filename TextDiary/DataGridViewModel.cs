81c34410fec46db02b2338902f1101fa3cabfc46
Akn <sibata486@gmail.com>
2019-04-28 02:08:07 +0900

    [クラス追加]　データグリッドビュー管理用のモデルを追加

---
+++ TextDiary/DataGridViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDiary {
+
+    public delegate void ListStateChanged();
+
+    class DataGridViewModel {
+        public event ListStateChanged listStateChanged;
+    }
+}
