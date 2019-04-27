ad9ac5c2370d66f7a6f6e6060d72b02741e10bc5
Akn <sibata486@gmail.com>
2019-04-28 01:04:16 +0900

    [クラス追加]　MVCのコントローラー部に当たるクラスを追加

---
+++ TextDiary/MainFormController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDiary {
+
+    //MVCにおけるコントローラーの役割をするクラス
+    //UIに依存しない処理を行うモデルのインスタンスを保持。
+    //ビューであるメインフォームのインスタンスを監視するためにそのインスタンスも保持。
+
+    class MainFormController {
+    }
+}
