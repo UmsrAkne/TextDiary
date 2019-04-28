7917633d8ebb688f2ba8d0407ad60b25895ddf07
Akn <sibata486@gmail.com>
2019-04-28 12:56:49 +0900

    [クラス追加]　フォーム内のビューの情報を詰めたクラスを追加

---
+++ TextDiary/FormModelView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace TextDiary {
+    class FormModelView {
+
+        public string inputText = "";
+
+        public BindingList<Todo> todoList = new BindingList<Todo>();
+        public Point currentCellAdoress;
+    }
+}
