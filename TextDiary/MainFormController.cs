773c8b7fb7ab874c35d835e1181a162a0022265c
Akn <sibata486@gmail.com>
2019-04-28 13:05:43 +0900

    コントローラーにビューの状態を監視する処理を追加

--- TextDiary/MainFormController.cs
+++ TextDiary/MainFormController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace TextDiary {
 
@@ -10,6 +12,17 @@ namespace TextDiary {
     //UIに依存しない処理を行うモデルのインスタンスを保持。
     //ビューであるメインフォームのインスタンスを監視するためにそのインスタンスも保持。
 
-    class MainFormController {
+    public class MainFormController {
+
+        Form1 form;
+
+        public MainFormController( Form1 form) {
+            this.form = form;
+            form.dataGridViewKeyboardEventHandler += dgvKeyboardEventHandler;
+        }
+
+        private void dgvKeyboardEventHandler(object sender , KeyEventArgs e) {
+
+        }
     }
 }
