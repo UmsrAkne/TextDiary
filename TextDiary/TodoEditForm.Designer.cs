87211838a27e58c4960ec8b14429e2840dee2e4c
Akn <sibata486@gmail.com>
2019-05-24 12:50:15 +0900

    Todo編集フォームを追加

---
+++ TextDiary/TodoEditForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TextDiary {
+    partial class TodoEditForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            Sgry.Azuki.FontInfo fontInfo1 = new Sgry.Azuki.FontInfo();
+            this.textEditWindow = new Sgry.Azuki.WinForms.AzukiControl();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textEditWindow
+            // 
+            this.textEditWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(255)))));
+            this.textEditWindow.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textEditWindow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textEditWindow.DrawingOption = ((Sgry.Azuki.DrawingOption)((((((Sgry.Azuki.DrawingOption.DrawsFullWidthSpace | Sgry.Azuki.DrawingOption.DrawsTab) 
+            | Sgry.Azuki.DrawingOption.DrawsEol) 
+            | Sgry.Azuki.DrawingOption.HighlightCurrentLine) 
+            | Sgry.Azuki.DrawingOption.ShowsLineNumber) 
+            | Sgry.Azuki.DrawingOption.HighlightsMatchedBracket)));
+            this.textEditWindow.FirstVisibleLine = 0;
+            this.textEditWindow.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            fontInfo1.Name = "MS UI Gothic";
+            fontInfo1.Size = 12;
+            fontInfo1.Style = System.Drawing.FontStyle.Regular;
+            this.textEditWindow.FontInfo = fontInfo1;
+            this.textEditWindow.ForeColor = System.Drawing.Color.Black;
+            this.textEditWindow.Location = new System.Drawing.Point(0, 0);
+            this.textEditWindow.Name = "textEditWindow";
+            this.textEditWindow.ScrollPos = new System.Drawing.Point(0, 0);
+            this.textEditWindow.ShowsDirtBar = false;
+            this.textEditWindow.Size = new System.Drawing.Size(484, 115);
+            this.textEditWindow.TabIndex = 0;
+            this.textEditWindow.Text = "default";
+            this.textEditWindow.ViewWidth = 4135;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 147);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.dateTimePicker2.Location = new System.Drawing.Point(218, 147);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 121);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Complete";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // TodoEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 212);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textEditWindow);
+            this.Name = "TodoEditForm";
+            this.Text = "TodoEditForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Sgry.Azuki.WinForms.AzukiControl textEditWindow;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
