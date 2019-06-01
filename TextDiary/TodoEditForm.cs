ef7e394dc577687e8087f29de6c10e732655855f
Akn <sibata486@gmail.com>
2019-06-02 00:45:08 +0900

    バグ修正　DatePickerにDateTimeの最小値を入力することがないよう修正

--- TextDiary/TodoEditForm.cs
+++ TextDiary/TodoEditForm.cs
@@ -40,7 +40,6 @@ namespace TextDiary {
             currentTodo = todo;
             textEditWindow.Text = todo.content;
             additionDatePicker.Value = todo.additionDate;
-            completeDatePicker.Value = todo.completedDate;
 
             if(todo.completedDate == DateTime.MinValue) {
                 completeDatePicker.Value = DateTimePicker.MinimumDateTime;
